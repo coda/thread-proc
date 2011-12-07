b24e69bcfa2cbab9c391732cfcf8a3f0cddedbe7
mob <mike.bakhterev@gmail.com>
2011-12-07 21:48:17 +0600

    proc with anonymous mmap. alfa

---
+++ matmul/naiveproc.c
@@ -0,0 +1,153 @@
+#include <./mul.h>
+#include <./util.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <errno.h>
+#include <unistd.h>
+
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <linux/mman.h>
+
+static struct
+{
+	eltype * a;
+	eltype * b;
+	eltype * r;
+
+ 	testconfig cfg;
+} setup;
+
+
+static void runjobs(const unsigned count, void (* routine)(const unsigned))
+{
+	pid_t procs[count];
+	unsigned ok = 1;
+	
+	for(unsigned i = 0; ok && i < count; i += 1)
+	{
+		pid_t p = fork();
+		if(p == 0)
+		{
+			routine(i);
+			exit(0);
+		}
+		if(p > 0)
+		{
+			procs[i] = p;
+		}
+		else
+		{
+			ok = 0;
+		}
+	}
+
+	if(ok) {} else
+	{
+		eprintf("err: %s. can't start %u jobs\n",
+			strerror(errno), count);
+		exit(1);
+	}
+
+	for(unsigned i = 0; ok && i < count; i += 1)
+	{
+		ok = waitpid(procs[i], NULL, 0) == procs[i];
+	}
+
+	if(ok) {} else
+	{
+		eprintf("err: %s. can't join %u jobs\n",
+			strerror(errno), count);
+		exit(1);
+	}
+}
+
+static void randroutine(const unsigned id)
+{
+	const unsigned sz = setup.cfg.size;
+	const unsigned nwrks = setup.cfg.nworkers; 
+
+	const unsigned m = sz;
+	const unsigned n = sz;
+
+	const jobitem ji = ballance(id, nwrks, sz);
+	const unsigned l = ji.nrows;
+	const unsigned sr = ji.startrow;
+	
+	eltype *const a = setup.a + sr * m;
+	eltype *const b = setup.b + sr * n;
+
+	matrand(id, a, l, m);
+	matrand(id * 5, b, l, n);
+
+	printf("rand %u with %u rows is done\n", id, l);
+}
+
+static void multroutine(const unsigned id)
+{
+	const unsigned sz = setup.cfg.size;
+	const unsigned nwrks = setup.cfg.nworkers; 
+
+	const unsigned m = sz;
+	const unsigned n = sz;
+
+	const jobitem ji = ballance(id, nwrks, sz);
+	const unsigned l = ji.nrows;
+	const unsigned sr = ji.startrow;
+	
+	const eltype *const a = setup.a + sr * m;
+	eltype *const r = setup.r + sr * n;
+	
+	matmul(a, setup.b, l, m, n, r);
+
+	printf("mult %u with %u rows is done\n", id, l);
+}
+
+static void * matalloc(const unsigned m, const unsigned n)
+{
+	const long plen = sysconf(_SC_PAGESIZE);
+	if(plen > 0 && plen < (1 << 30)) {} else
+	{
+		eprintf("err: %s. can't get sane page size. plen: %ld\n",
+			strerror(errno), plen);
+		exit(1);
+	}
+
+	const unsigned len = align(m * n * sizeof(eltype), plen);
+	void * ptr = mmap(NULL, len, PROT_WRITE | PROT_READ,
+		MAP_ANONYMOUS | MAP_SHARED | MAP_UNINITIALIZED, -1, 0);
+	if(ptr != MAP_FAILED) {} else
+	{
+		eprintf("err: %s. can't allocate %u bytes\n", len);
+		exit(1);
+	}
+	
+	return ptr;
+}
+
+int main(int argc, const char *const *const argv)
+{
+	setup.cfg = fillconfig(argc, argv);
+	const unsigned sz = setup.cfg.size;
+	const unsigned nw = setup.cfg.nworkers;
+
+	printf("nworkers: %u; matrix size: %fMiB\n",
+		nw, (double)sz * sz * sizeof(eltype) / (double)(1 << 20));
+
+	setup.a = matalloc(sz, sz);
+	setup.b = matalloc(sz, sz);
+	setup.r = matalloc(sz, sz);
+
+	printf("allocated\n");
+
+	printf("randomization\n"); fflush(stdout); fflush(stderr);
+	runjobs(nw, randroutine);
+
+	printf("multiplication\n"); fflush(stdout); fflush(stderr);
+	runjobs(nw, multroutine);
+
+	return 0;
+}
