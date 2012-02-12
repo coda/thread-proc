66993996a3fc66e34c0590c3519c90d0f2d9e6a8
mob <mike.bakhterev@gmail.com>
2012-02-13 00:35:36 +0600

    src/gnu.mk added

---
+++ src/gnu.mk
@@ -0,0 +1,12 @@
+include $(foreign)/mkenv/gnu/common.mk
+
+cflags += -D_POSIX_C_SOURCE=200809 -D_GNU_SOURCE
+
+all: $(bld)/bin/runall exchg matmul alloc util
+
+$(bld)/bin/runall: runall.sh
+
+include util/gnu.mk
+include alloc/gnu.mk
+include matmul/gnu.mk
+include exchg/gnu.mk
